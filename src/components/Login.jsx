abcfc6695ed501e33c8bbc075b9cea43ebeb09b8
loicbdev <loicbodolec@msn.com>
2021-01-11 12:36:12 +0100

    login fonctionnel

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import useStyles from "./styleFormConnection";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -12,35 +15,44 @@ const Login = () => {
     }
   };
 
+  const classes = useStyles();
+
   return (
-    <form>
-      <label htmlFor="email">
-        Email:
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="test@blabla.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <br />
-      <label htmlFor="password">
-        Password:
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="***********"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button type="button" onClick={handleSubmit}>
-        Login
-      </button>
+    <form className={classes.form} noValidate>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        type="email"
+        label="Email"
+        className={classes.someInput}
+        name="email"
+        autoComplete="email"
+        autoFocus
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        name="password"
+        id="password"
+        placeholder="***********"
+        variant="outlined"
+        className={classes.someInput}
+        margin="normal"
+        fullWidth
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button
+        text="Connexion"
+        color="primary"
+        size="medium"
+        variant="contained"
+        fullWidth
+        onClick={handleSubmit}
+      />
     </form>
   );
 };
