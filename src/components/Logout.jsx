08079f10b544e5605c89de5839b9538f5b13fe69
loicbdev <loicbodolec@msn.com>
2021-01-11 15:32:24 +0100

    CSS style on logout button

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import useStyles from "./styleFormConnection";
 
 const Logout = () => {
   const handleSubmit = () => {
@@ -7,8 +8,10 @@ const Logout = () => {
     alert("Disconnected successfully");
   };
 
+  const classes = useStyles();
+
   return (
-    <form>
+    <form className={classes.form}>
       <Button
         variant="contained"
         color="primary"
