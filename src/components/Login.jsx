3d9ab8e4f2926172c4fea68a1999a4b5196f218b
loicbdev <loicbodolec@msn.com>
2021-01-11 11:36:04 +0100

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  // Your code here
-  return <form />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    if (email && password) {
+      console.log(email, password);
+    } else {
+      alert("Please specify both email and password");
+    }
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="test@blabla.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label htmlFor="password">
+        Password:
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="***********"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="button" onClick={handleSubmit}>
+        Login
+      </button>
+    </form>
+  );
 };
 
 export default Login;
